c907f990216aebb3c9ca9113c665c10c0ab69f4a
JamesleeG <jamesleegambardella@gmail.com>
2021-03-23 14:22:27 -0400

    Created crud pathways and picture index route should be finished may need a link

---
+++ src/api/Pictures.js
@@ -0,0 +1,43 @@
+import axios from 'axios'
+import apiUrl from '../apiConfig'
+
+export const pictureIndex = user => {
+  return axios({
+    url: apiUrl + '/pictures',
+    method: 'GET',
+    // include an authorization header, that includes our user's token
+    // so the API knows who to sign out
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+
+export const pictureShow = (user, id) => {
+  return axios({
+    url: apiUrl + '/pictures/' + id,
+    method: 'Get',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+export const pictureDelete = (user, id) => {
+  return axios({
+    url: apiUrl + '/pictures/' + id,
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    }
+  })
+}
+export const pictureUpdate = (id, picture, user) => {
+  return axios({
+    url: apiUrl + '/pictures/' + id,
+    method: 'PATCH',
+    headers: {
+      'Authorization': `Token token=${user.token}`
+    },
+    data: { picture }
+  })
+}
