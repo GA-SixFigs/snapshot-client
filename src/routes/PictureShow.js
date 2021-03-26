4de2a9051106321d2d36e68bfa2972ff721f70ac
cmd08010 <colleendunion@gmail.com>
2021-03-26 16:34:48 -0400

    Add date updated

--- src/routes/PictureShow.js
+++ src/routes/PictureShow.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Redirect, withRouter } from 'react-router-dom'
 import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
+import moment from 'moment'
 // import Form from 'react-bootstrap/Form'
 // import axios from 'axios'
 // import apiUrl from '../../apiConfig'
@@ -136,7 +137,7 @@ class ShowPicture extends Component {
             <h5>Owner: {picture.ownerName}</h5>
             <h5>Caption: {picture.caption}</h5>
             <h5>Tag: {picture.tag}</h5>
-            <h5>Created: {picture.createdAt}</h5>
+            <h5>Created: {moment(picture.createdAt).format('ddd, hA ')}</h5>
             <br />
             <Button variant='primary' onClick={this.deletePicture}>Delete Me</Button>
             <br />
