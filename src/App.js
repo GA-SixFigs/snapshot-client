6d137772bc4712f0c9a34a1ed3fc9be2af425b07
cmd08010 <colleendunion@gmail.com>
2021-03-26 18:38:37 -0400

    set default page to gallery

--- src/App.js
+++ src/App.js
@@ -96,6 +96,9 @@ class App extends Component {
           <Route path='/gallery' render={() => (
             <Gallery msgAlert={this.msgAlert} />
           )} />
+          <Route exact path='/' render={() => (
+            <Gallery msgAlert={this.msgAlert} />
+          )} />
           <AuthenticatedRoute user={user} path='/sign-out' render={() => (
             <SignOut msgAlert={this.msgAlert} clearUser={this.clearUser} user={user} />
           )} />
